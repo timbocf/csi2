68d19e255fa254372503324670c734ae13c466d8
Tim Rodgers <timbocf@gmail.com>
2014-09-02 12:22:44 -0700

    added bootstrap navigation

--- header.php
+++ header.php
@@ -128,10 +128,40 @@
 	</div>
 
 	<div class="navigation">
+		<div class="container">
+			<div class="col-xs-12">
+				<header class="navbar navbar-inverse bs-docs-nav" role="banner">
 
-		<div></div>
+					<div class="navbar-header">
+						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
 
-	</div>
+					<a href="home"><img src="<?php bloginfo('template_directory') ?>/images/trd-logo.png" class="logo img-responsive"></a>
+
+					<nav class="collapse navbar-collapse bs-navbar-collapse col-xs-12" role="navigation">
+						
+						<ul class="nav navbar-nav row">
+							<li><a href="home" class="active first" id="home">Home</a></li>
+							<li><a href="about-us" id="about-us">About Us</a></li>
+							<li><a href="services" id="services">Services</a></li>
+							<li><a href="portfolio" id="portfolio">Portfolio</a></li>
+							<li><a href="contact-us" id="contact-us">Contact Us</a></li>
+							<li class="social">
+								<a href="//facebook.com" class="icon-social-facebook">Facebook</a>
+								<a href="//twitter.com" class="icon-social-twitter">Twitter</a>
+							</li>
+						</ul>
+					</nav>
+					
+				</header>
+			</div><!-- col-xs-12 -->
+		</div><!-- container -->
+	</div><!-- .navigation -->
 
 	<div id="content" class="site-content">
 

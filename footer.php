930de7412fc0bba0da2cd713c6e5f4efbe24971a
Tim Rodgers <timbocf@gmail.com>
2014-09-03 09:07:28 -0700

    tweaking spacing issues

--- footer.php
+++ footer.php
@@ -10,43 +10,49 @@
 
 		</div><!-- container -->
 
+		<div class="container">
+
+			<footer id="colophon" class="site-footer">
+				<div class="col-md-2">
+					<a href="/privacy-policy/">Privacy Policy</a>
+					<span class="sep"> | </span>
+					<a href="/site-map/">Site Map</a>
+				</div>
+
+				<div class="col-md-8">
+					<ul>
+						<li>Proud Members Of:</li>
+						<li>
+							<a href="#todo">
+								<img src="#todo">
+							</a>
+						</li>
+						<li>
+							<a href="#todo">
+								<img src="#todo">
+							</a>
+						</li>
+						<li>
+							<a href="#todo">
+								<img src="#todo">
+							</a>
+						</li>
+						<li>
+							<a href="#todo">
+								<img src="#todo">
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-md-2">
+					<a href="http://5pints.com">Site by 5 Pints Productions</a>
+				</div>
+			</footer><!-- #colophon -->
+
+		</div><!-- container -->
+
 	</div><!-- #content -->
-
-	<div class="container">
-
-		<footer id="colophon" class="site-footer">
-			<div class="col-md-2">
-				<a href="/privacy-policy/">Privacy Policy</a>
-				<span class="sep"> | </span>
-				<a href="/site-map/">Site Map</a>
-				<ul>
-					<li>Proud Members Of:</li>
-					<li>
-						<a href="#todo">
-							<img src="#todo">
-						</a>
-					</li>
-					<li>
-						<a href="#todo">
-							<img src="#todo">
-						</a>
-					</li>
-					<li>
-						<a href="#todo">
-							<img src="#todo">
-						</a>
-					</li>
-					<li>
-						<a href="#todo">
-							<img src="#todo">
-						</a>
-					</li>
-				</ul>
-				<a href="http://5pints.com">Site by 5 Pints Productions</a>
-			</div>
-		</footer><!-- #colophon -->
-
-	</div><!-- container -->
 
 </div><!-- #page -->
 
--- page-home.php
+++ page-home.php
@@ -12,136 +12,154 @@
 
 		</div>
 
-		<div class="row slider-overlay">
+		<div class="container">
 
-			<div>Find Contractor Support</div>
+			<div class="slider-overlay">
 
-			<div>Find Skilled Craftsmen</div>
+				<div>Find Contractor Support</div>
 
-			<div class="last">Find a Dream Job</div>
+				<div>Find Skilled Craftsmen</div>
 
-		</div>
+				<div class="last">Find a Dream Job</div>
 
-	</div>
-
-	<div class="col-md-4">
-
-		<div class="left-sidebar">
-
-			<h3>Contractor Staffing Services</h3>
-
-			<ul>
-				<li>Contractors</li>
-				<li>Customer Satisfaction</li>
-				<li>Staffing Solutions</li>
-				<li>Safety</li>
-				<li>Partner With CSI</li>
-				<li>Construction Staffing States</li>
-				<li>Electrical Staffing</li>
-				<li>HVAC Mechanical Staffing</li>
-				<li>Plumbing Staffing</li>
-				<li>Welding Staffing</li>
-				<li>Carpentry Staffing</li>
-				<li>Concrete Staffing</li>
-				<li>Iron Work Staffing</li>
-				<li>Metal Work Staffing</li>
-			</ul>
-
-			<h3>Skilled Tradesmen</h3>
-
-			<ul>
-				<li>Tradesmen</li>
-				<li>CSI Promise to You</li>
-				<li>Employee Benefits</li>
-				<li>Traveling Tradesmen</li>
-				<li>Tradesmen Job Openings</li>
-				<li>Electricians Apply Online</li>
-				<li>HVAC Mechanical Apply Online</li>
-				<li>Carpenters Apply Online</li>
-				<li>Concrete Masons Apply Online</li>
-				<li>Plumbers Apply Online</li>
-				<li>Iron Workers Apply Online</li>
-				<li>Metal Workers Apply Online</li>
-				<li>Welders Apply Online</li>
-				<li>Helpers Apply Online</li>
-			</ul>
-
-			<h3>Construction Industries</h3>
-
-			<ul>
-				<li>Industrial Staffing</li>
-				<li>Renewable Energy Staffing</li>
-				<li>Institutional Staffing</li>
-				<li>Retail Construction Staffing</li>
-			</ul>
-
-		</div>
-
-	</div>
-
-	<div class="col-md-8">
-
-		<div class="home-bio">
-
-			<h1>Welcome to Construction Strategies, Inc.</h1>
-
-			<p>Construction Strategies, Inc. provides electricians and skilled trades people across the nation to fill large and small scale job openings in Commercial Construction, Industrial Construction, Institutional Construction, and Renewable Energy Projects.</p>
-
-			<p>Call CSI to Staff Your Next Project! Because we are tradesmen ourselves, we know how to hire high-quality skilled trades workers and how to deliver the best service and quality workmanship to our clients.</p>
-
-			<p>We deploy thousands of skilled trades employees throughout the United States each year.</p>
-
-			<p>Call CSI to Staff Your Next Project! Because we are tradesmen ourselves, we know how to hire high-quality skilled trades workers and how to deliver the best service and quality workmanship to our clients.</p>
-
-		</div>
-
-		<div class="testimonials">[Testimonials]</div>
-
-		<div class="home-services">
-
-			<h1>Top Quality Labor Support</h1>
-
-			<ul>
-				<li>Electricians</li>
-				<li>Sheet Metal</li>
-				<li>Iron Workers</li>
-				<li>Painters</li>
-				<li>HVAC Techs</li>
-				<li>Welders</li>
-				<li>Carpenters</li>
-			</ul>
-
-			<ul>
-				<li>Riggers</li>
-				<li>Plumbers</li>
-				<li>Concrete</li>
-				<li>Masons</li>
-				<li>Metal Workers</li>
-				<li>Pipe Fitters</li>
-			</ul>
-
-		</div>
-
-	</div>
-
-
-	<div class="row">
-
-		<div class="content-footer">
-
-			<div class="col-md-3">
-				<a href="#todo">Find Contractor Support</a>
 			</div>
-			<div class="col-md-3">
-				<a href="#todo">Find Skilled Craftsmen</a>
-			</div>
-			<div class="col-md-3">
-				<a href="#todo">Find a Dream Job</a>
-			</div>
-			<div class="col-md-3 last"></div>
 
 		</div>
 
 	</div>
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-md-4">
+
+				<div class="left-sidebar">
+
+					<h3>Contractor Staffing Services</h3>
+
+					<ul>
+						<li>Contractors</li>
+						<li>Customer Satisfaction</li>
+						<li>Staffing Solutions</li>
+						<li>Safety</li>
+						<li>Partner With CSI</li>
+						<li>Construction Staffing States</li>
+						<li>Electrical Staffing</li>
+						<li>HVAC Mechanical Staffing</li>
+						<li>Plumbing Staffing</li>
+						<li>Welding Staffing</li>
+						<li>Carpentry Staffing</li>
+						<li>Concrete Staffing</li>
+						<li>Iron Work Staffing</li>
+						<li>Metal Work Staffing</li>
+					</ul>
+
+					<h3>Skilled Tradesmen</h3>
+
+					<ul>
+						<li>Tradesmen</li>
+						<li>CSI Promise to You</li>
+						<li>Employee Benefits</li>
+						<li>Traveling Tradesmen</li>
+						<li>Tradesmen Job Openings</li>
+						<li>Electricians Apply Online</li>
+						<li>HVAC Mechanical Apply Online</li>
+						<li>Carpenters Apply Online</li>
+						<li>Concrete Masons Apply Online</li>
+						<li>Plumbers Apply Online</li>
+						<li>Iron Workers Apply Online</li>
+						<li>Metal Workers Apply Online</li>
+						<li>Welders Apply Online</li>
+						<li>Helpers Apply Online</li>
+					</ul>
+
+					<h3>Construction Industries</h3>
+
+					<ul>
+						<li>Industrial Staffing</li>
+						<li>Renewable Energy Staffing</li>
+						<li>Institutional Staffing</li>
+						<li>Retail Construction Staffing</li>
+					</ul>
+
+				</div>
+
+			</div>
+
+			<div class="col-md-8">
+
+				<div class="home-bio">
+
+					<h1>Welcome to Construction Strategies, Inc.</h1>
+
+					<p>Construction Strategies, Inc. provides electricians and skilled trades people across the nation to fill large and small scale job openings in Commercial Construction, Industrial Construction, Institutional Construction, and Renewable Energy Projects.</p>
+
+					<p>Call CSI to Staff Your Next Project! Because we are tradesmen ourselves, we know how to hire high-quality skilled trades workers and how to deliver the best service and quality workmanship to our clients.</p>
+
+					<p>We deploy thousands of skilled trades employees throughout the United States each year.</p>
+
+					<p>Call CSI to Staff Your Next Project! Because we are tradesmen ourselves, we know how to hire high-quality skilled trades workers and how to deliver the best service and quality workmanship to our clients.</p>
+
+				</div>
+
+				<div class="testimonials">[Testimonials]</div>
+
+				<div class="home-services">
+
+				<h1>Top Quality Labor Support</h1>
+
+					<div class="row">
+
+						<ul class="col-md-3">
+							<li>Electricians</li>
+							<li>Sheet Metal</li>
+							<li>Iron Workers</li>
+							<li>Painters</li>
+							<li>HVAC Techs</li>
+							<li>Welders</li>
+							<li>Carpenters</li>
+						</ul>
+
+						<ul class="col-md-3">
+							<li>Riggers</li>
+							<li>Plumbers</li>
+							<li>Concrete</li>
+							<li>Masons</li>
+							<li>Metal Workers</li>
+							<li>Pipe Fitters</li>
+						</ul>
+
+						<img src="#todo" class="col-md-6">
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="clearfix"></div>
+
+		</div><!-- row -->
+
+	</div><!-- container -->
+
+
+	<div class="content-footer">
+
+		<div class="col-md-3">
+			<a href="#todo">Find Contractor Support</a>
+		</div>
+		<div class="col-md-3">
+			<a href="#todo">Find Skilled Craftsmen</a>
+		</div>
+		<div class="col-md-3">
+			<a href="#todo">Find a Dream Job</a>
+		</div>
+		<div class="col-md-3 last"></div>
+
+	</div>
+
+
 
 <?php get_footer(); ?>
